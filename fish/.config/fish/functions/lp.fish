6ef81c56b60eec6a7efefaf28c598f5c647adc85
Praveen Perumal <ppraveen98841@gmail.com>
2022-06-04 20:35:54 +0530

    ⚡Added . functionality to lp

--- fish/.config/fish/functions/lp.fish
+++ fish/.config/fish/functions/lp.fish
@@ -1,7 +1,11 @@
 # lp -> list projects
 function lp
-  set folder (find ~/Code ~/.dotfiles2 ~/.dotfiles2/fish/.config/fish  -maxdepth 1 -type d | fzf)
-  if [ "$folder" ]
-    tmux new-session -s (basename $folder) -c $folder
+  if [ "$argv[1]" = "." ]
+    tmux new-session -s (basename $PWD) -c .
+  else
+    set folder (find ~/Code ~/.dotfiles2 ~/.dotfiles2/fish/.config/fish  -maxdepth 1 -type d | fzf)
+    if [ "$folder" ]
+      tmux new-session -s (basename $folder) -c $folder
+    end
   end
 end
