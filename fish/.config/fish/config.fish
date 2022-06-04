cb5cc73b9d0b291929eb01640eb73137896a44c8
Praveen Perumal <ppraveen98841@gmail.com>
2022-06-04 16:37:43 +0530

    ⚡Added alias for exa, lazygit, git,..

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,1 +1,15 @@
-set PATH /opt/homebrew/bin $PATH
+# Add to PATH
+set PATH $PATH /opt/homebrew/bin
+
+# Alias
+alias l="exa -al -s=type --icons --no-user"
+alias lg="lazygit"
+
+alias gs="git status"
+alias gc="git commit"
+alias gl="git log --oneline --graph"
+alias gp="git push"
+alias gu="git restore"
+
+alias bd="cd ~/.dotfiles2"
+alias bc="cd ~/Code"
