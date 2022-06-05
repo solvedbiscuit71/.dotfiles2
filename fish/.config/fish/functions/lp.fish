52ea339c55fb83be64c0cf7820cd001f0a2fcf64
Praveen Perumal <ppraveen98841@gmail.com>
2022-06-05 18:01:45 +0530

    ✨ Added functionality to work inside tmux

--- fish/.config/fish/functions/lp.fish
+++ fish/.config/fish/functions/lp.fish
@@ -1,11 +1,29 @@
 # lp -> list projects
 function lp
+  # check for `lp .`
   if [ "$argv[1]" = "." ]
-    tmux new-session -s (basename $PWD) -c .
+    set folder $PWD
   else
     set folder (find ~/Code ~/.dotfiles2 ~/.dotfiles2/fish/.config/fish  -maxdepth 1 -type d | fzf)
-    if [ "$folder" ]
-      tmux new-session -s (basename $folder) -c $folder
+  end
+
+  if [ "$folder" ]
+    set folder_name (basename $folder)
+    tmux has-session -t=$folder_name 2> /dev/null
+
+    if [ "$status" = "0" ]
+      if [ "$TMUX" = "" ]
+        tmux attach -t $folder_name
+      else
+        tmux switch-client -t $folder_name
+      end
+    else
+      if [ "$TMUX" = "" ]
+        tmux new-session -s $folder_name -c $folder
+      else
+        tmux new-session -ds $folder_name -c $folder
+        tmux switch-client -t $folder_name
+      end
     end
   end
 end
