9074d39bc457d152474f2484e027a35309e84d44
Praveen Perumal <ppraveen98841@gmail.com>
2022-06-10 18:59:42 +0530

    ⚡Added some alias

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,8 +4,10 @@ set PATH $PATH /opt/homebrew/bin $HOME/.cargo/bin
 # Alias
 alias l="exa -al -s=type --icons --no-user"
 alias lg="lazygit"
+alias q="exit 0"
 
 alias gs="git status"
+alias ga="git add"
 alias gc="git commit"
 alias gl="git log --oneline --graph"
 alias gp="git push"
