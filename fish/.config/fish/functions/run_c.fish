dd70c885933329b6ad31a9d0ab9254d5debb343a
Praveen Perumal <ppraveen98841@gmail.com>
2022-06-14 19:30:00 +0530

    ⚡Renamed to run_c

--- fish/.config/fish/functions/run_c.fish
+++ fish/.config/fish/functions/run_c.fish
@@ -1,16 +1,16 @@
-function run
+function run_c
   set -l options (fish_opt -s g -l debug)
   argparse $options -- $argv
 
   if set -q _flag_debug
-    clang++ "$argv[1]" -g 2> /dev/null
+    clang++ -std=c++17 "$argv[1]" -g
 
     if [ $status = "0" ]
       lldb ./a.out
       rm a.out; rm -r a.out.dSYM
     end
   else
-    clang++ "$argv[1]" 2> /dev/null
+    clang++ -std=c++17 "$argv[1]"
 
     if [ $status = "0" ]
       ./a.out;
