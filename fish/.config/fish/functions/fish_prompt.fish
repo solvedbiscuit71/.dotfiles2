eaa993cf8d93bb1d1b40a9f9c3746f35c66cceaf
Praveen Perumal <ppraveen98841@gmail.com>
2022-08-03 15:49:54 +0530

    Refactor custom fish_prompt function

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,8 +1,14 @@
 function fish_prompt
     set -l __last_command_exit_status $status
 
-    if not set -q -g __fish_arrow_functions_defined
-        set -g __fish_arrow_functions_defined
+    if not set -q -g __fish_prompt_utils_defined
+        set -g __fish_prompt_utils_defined
+        function _is_git_repo
+            type -q git
+            or return 1
+            git rev-parse --git-dir >/dev/null 2>&1
+        end
+
         function _git_branch_name
             set -l branch (git symbolic-ref --quiet HEAD 2>/dev/null)
             if set -q branch[1]
@@ -11,50 +17,20 @@ function fish_prompt
                 echo (git rev-parse --short HEAD 2>/dev/null)
             end
         end
-
-        function _is_git_repo
-            type -q git
-            or return 1
-            git rev-parse --git-dir >/dev/null 2>&1
-        end
-
-        function _repo_branch_name
-            _$argv[1]_branch_name
-        end
-
-        function _repo_type
-            if _is_git_repo
-                echo git
-                return 0
-            end
-            return 1
-        end
     end
 
-    set -l cyan (set_color -o cyan)
-    set -l yellow (set_color -o yellow)
-    set -l red (set_color -o red)
-    set -l green (set_color -o green)
     set -l blue (set_color -o blue)
+    set -l cyan (set_color -o cyan)
     set -l normal (set_color normal)
 
-    set -l arrow_color "$green"
-    if test $__last_command_exit_status != 0
-        set arrow_color "$red"
-    end
-
-    set -l arrow "$arrow_color➜ "
-    if fish_is_root_user
-        set arrow "$arrow_color# "
-    end
-
-    set -l cwd $cyan(basename (prompt_pwd))
+    set -l arrow '$'
+    set -l cwd (basename (prompt_pwd))
 
     set -l repo_info
-    if set -l repo_type (_repo_type)
-        set -l repo_branch $blue(_repo_branch_name $repo_type)
-        set repo_info " $blue($repo_branch$blue)"
+    if _is_git_repo
+        set -l repo_branch (_git_branch_name)
+        set repo_info "($repo_branch) "
     end
 
-    echo -n -s $arrow ' '$cwd $repo_info $normal ' '
+    echo -n -s $cyan $cwd ' '$blue $repo_info $normal $arrow ' '
 end
