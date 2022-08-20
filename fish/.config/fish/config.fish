a4d68350cd69aa647f205c387e47515a7236e389
Praveen Perumal <solvedbiscuit71@Praveens-MacBook-Air.local>
2022-08-20 22:24:21 +0530

    Alias bd, bw, bc in fish shell

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,11 +1,15 @@
 # Add to PATH
 set PATH "/opt/homebrew/bin/" "$HOME/.cargo/bin/" $PATH
 
-# Alias
+# Alias to external commands
 alias l="exa -al -s=type --no-user"
 alias lg="lazygit"
+alias vim="nvim"
+
+# Alias to builtin command
 alias q="exit 0"
 
+# Alias to git command
 alias gs="git status"
 alias ga="git add"
 alias gc="git commit"
@@ -14,8 +18,10 @@ alias gd="git diff"
 alias gp="git push"
 alias gu="git restore"
 
-alias vim="nvim"
-alias config="cd ~/.dotfiles2 && lp ."
+# Bookmark alias
+alias bd="cd ~/.dotfiles2"
+alias bc="cd ~/code"
+alias bw="cd ~/.wiki && nvim README.md"
 
 # Bun
 set --export BUN_INSTALL "$HOME/.bun"
