77c74677ddfc1b66695f09fbe5650e29809a2dd6
Praveen Perumal <ppraveen98841@gmail.com>
2022-09-20 11:28:29 +0530

    Move 'markdown' from disable to ensure_installed

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,11 +1,10 @@
 require('nvim-treesitter.configs').setup {
-    ensure_installed = {'lua', 'fish', 'vim', 'yaml'},
+    ensure_installed = {'lua', 'fish', 'vim', 'yaml', 'markdown'},
     sync_install = true,
     auto_install = true,
 
     highlight = {
         enable = true,
-        disable = {'markdown'},
         additional_vim_regex_highlighting = false
     }
 }
