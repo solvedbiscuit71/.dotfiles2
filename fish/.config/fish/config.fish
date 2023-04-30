75bfdeb7458456953412b1b7713a5270966db7ed
Praveen Perumal <ppraveen98841@gmail.com>
2023-04-30 22:22:04 +0530

    Added alias for ls

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,6 +2,7 @@ set PATH "/opt/homebrew/bin/" $PATH
 
 alias vim="nvim"
 alias lg="lazygit"
+alias ls="exa -l -s type --no-user --git"
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
