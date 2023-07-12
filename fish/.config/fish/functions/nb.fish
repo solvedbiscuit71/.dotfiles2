e64c674ca38f8e6ba5f706dccdefff80e3d2c68b
Praveen Perumal <ppraveen98841@gmail.com>
2023-07-12 15:18:21 +0530

    Update nb.fish

--- fish/.config/fish/functions/nb.fish
+++ fish/.config/fish/functions/nb.fish
@@ -1,3 +1,6 @@
 function nb
+    echo "running jupyter notebook at http://localhost:8888/tree"
+    echo "press ^C to terminate"
+
     jupyter notebook --no-browser --notebook-dir='~' -y &> /dev/null
 end
