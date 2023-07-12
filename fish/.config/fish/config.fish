8e2f7358ad372a321de6e0097182975b8a608b8f
Praveen Perumal <ppraveen98841@gmail.com>
2023-07-12 15:22:56 +0530

    Update fish.conf

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,11 @@
-set PATH "/opt/homebrew/bin/" $PATH
+set PATH "/opt/homebrew/bin/" "$HOME/.dotfiles2/bin/" $PATH
 
-alias vim="nvim"
+alias vi="nvim"
+alias re="ranger"
 alias lg="lazygit"
+
 alias ls="exa -l -s type --no-user --git"
+alias q="exit"
+
+# ghcup-env
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $HOME/.ghcup/bin $PATH
