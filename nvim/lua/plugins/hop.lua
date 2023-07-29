f36d870c51add86465b7b526d1463350f56ddb30
Praveen Perumal <ppraveen98841@gmail.com>
2023-07-29 20:48:44 +0530

    Remove keymappings
    Since f,F,t,T are default keymap overwriting them
    caused some issues.

--- nvim/lua/plugins/hop.lua
+++ nvim/lua/plugins/hop.lua
@@ -5,17 +5,4 @@ hop.setup {
     multi_windows = true,
 }
 
-vim.keymap.set('', 'f', function()
-    hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
-end, { remap = true })
-vim.keymap.set('', 'F', function()
-    hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
-end, { remap = true })
-vim.keymap.set('', 't', function()
-    hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
-end, { remap = true })
-vim.keymap.set('', 'T', function()
-    hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
-end, { remap = true })
-
 vim.keymap.set('n', '<leader>c', hop.hint_char1, { silent = true })
