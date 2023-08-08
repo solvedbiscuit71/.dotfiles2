49df72113f2f88da1dcf56dac968f5dcddf79ec6
Praveen Perumal <ppraveen98841@gmail.com>
2023-08-08 18:04:34 +0530

    Fix typo
    incorrect usage of opt instead of using opts

--- nvim/lua/keybinds.lua
+++ nvim/lua/keybinds.lua
@@ -8,5 +8,5 @@ vim.keymap.set('i', 'jj', '<ESC>', opts)
 vim.keymap.set('n', '<C-b>', '<C-^>', opts)
 vim.keymap.set({'n','v'}, '<leader>y', '"*y', opts)
 
-vim.keymap.set('v', '>', '>gv', opts)
+vim.keymap.set('v', '>', '>gv', opt)
 vim.keymap.set('v', '<', '<gv', opts)
