43985359b4690c3845a8985206c362f86227c88a
Praveen Perumal <ppraveen98841@gmail.com>
2023-09-28 23:12:32 +0530

    Added <C-a> keymap
    <C-a> is a text object which select the entire file

--- nvim/lua/keybinds.lua
+++ nvim/lua/keybinds.lua
@@ -6,7 +6,7 @@ vim.g.mapleader = ' '
 vim.keymap.set('i', 'jj', '<ESC>', opts)
 
 vim.keymap.set('n', '<C-b>', '<C-^>', opts)
-vim.keymap.set({'n','v','o'}, 'A', ':<C-u>normal! ggVG<CR>', opts)
+vim.keymap.set({'n','v','o'}, '<C-a>', ':<C-u>normal! ggVG<CR>', opts)
 
 vim.keymap.set('v', '>', '>gv', opts)
 vim.keymap.set('v', '<', '<gv', opts)
