dcceb19594630e8699225597a29d72f8d24befc3
Praveen Perumal <ppraveen98841@gmail.com>
2023-09-28 21:47:08 +0530

    Refactor config.fish
    Moved specific configuration into custom_config.fish
    which will not be managed by GIT

--- fish/config.fish
+++ fish/config.fish
@@ -1,17 +1,9 @@
 # path configuration
-set PATH "/opt/homebrew/bin/" "$HOME/.dotfiles2/.script/" $PATH
+set -gx PATH /opt/homebrew/bin/ $HOME/.dotfiles2/.script/ $PATH
 set -gx VISUAL "nvim"
 
 # jump configuration
 jump shell fish | source
-
-# ghcup configuration
-set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME
-set -gx PATH $HOME/.cabal/bin $HOME/.ghcup/bin $PATH
-
-# opam configuration
-source $HOME/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
-eval $(opam env)
 
 # aliases
 alias v="nvim"
@@ -19,6 +11,11 @@ alias g="lazygit"
 
 alias l="exa -1 --no-user -s name -s type"
 alias q="exit"
+
+# load add-ons
+if test -e $HOME/.config/fish/custom_config.fish
+    source $HOME/.config/fish/custom_config.fish
+end
 
 # add newline post execution
 function postexec_test --on-event fish_postexec
