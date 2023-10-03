d34f2849a26a7a83ca8c1e365b7af1f7436bd344
Praveen Perumal <ppraveen98841@gmail.com>
2023-10-03 17:13:33 +0530

    Remove <C-a> keybind
    As it collide with the vim default for incrementing numbers

--- nvim/lua/keybinds.lua
+++ nvim/lua/keybinds.lua
@@ -4,9 +4,8 @@ local opts = { silent = true }
 vim.g.mapleader = ' '
 
 vim.keymap.set('n', '<C-b>', '<C-^>', opts)
-vim.keymap.set({'n','v'}, '<C-a>', ':<C-u>normal! ggVG<CR>', opts)
 
 vim.keymap.set('v', '>', '>gv', opts)
 vim.keymap.set('v', '<', '<gv', opts)
 
-vim.keymap.set('o', 'A', ':<C-u>normal! ggVG<CR>', opts)
+vim.keymap.set({'o','x'}, 'A', ':<C-u>normal! ggVG<CR>', opts)
