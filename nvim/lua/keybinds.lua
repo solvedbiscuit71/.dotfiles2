2e45cb4907ab501aea3fb2ea4290c14aa7c6e1f7
Praveen Perumal <ppraveen98841@gmail.com>
2023-10-22 14:05:29 +0530

    Major Update 🔥

--- nvim/lua/keybinds.lua
+++ nvim/lua/keybinds.lua
@@ -4,8 +4,12 @@ local opts = { silent = true }
 vim.g.mapleader = ' '
 
 vim.keymap.set('n', '<C-b>', '<C-^>', opts)
+vim.keymap.set('n', '<leader>q', ':qa<CR>', opts)
+vim.keymap.set('n', '<leader>w', ':wa<CR>', opts)
+vim.keymap.set('n', '<leader>d', ':bd!<CR>', opts)
+
 
 vim.keymap.set('v', '>', '>gv', opts)
 vim.keymap.set('v', '<', '<gv', opts)
 
-vim.keymap.set({'o','x'}, 'A', ':<C-u>normal! ggVG<CR>', opts)
+vim.keymap.set({'n','o','x'}, '<leader>a', ':<C-u>normal! ggVG<CR>', opts)
